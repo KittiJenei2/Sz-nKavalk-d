60b02765af29439f63a13859f60b2699cf7fa250
Kitti <152151102+KittiJenei2@users.noreply.github.com>
2024-09-09 10:50:05 +0200

    mukodik

--- Program.cs
+++ Program.cs
@@ -10,6 +10,62 @@ namespace szinkavalkad
 	{
 		static void Main(string[] args)
 		{
+			List<string> alapSzinek = new List<string> { "piros", "kék", "sárga", "zöld" };
+
+			Random rnd = new Random();
+			List<string> feladvany = new List<string>();
+			while(feladvany.Count < 3)
+			{
+				string color = alapSzinek[rnd.Next(alapSzinek.Count)];
+				if(!feladvany.Contains(color))
+				{
+					feladvany.Add(color);
+				}
+			}
+
+            Console.WriteLine("Találd ki a 3 színt a helyes sorrendben!");
+			Console.WriteLine("Választható színek: piros, kék, zöld, sárga");
+            Console.WriteLine("??   ??   ??");
+            Console.WriteLine();
+
+            bool talalt = false;
+			while(!talalt)
+			{
+                Console.WriteLine();
+                Console.WriteLine("Add meg a választott 3 színt (szóközzel elválasztva):");
+				string tipp = Console.ReadLine().ToLower();
+				string[] szinTipp = tipp.Split(' ');
+
+				if(szinTipp.Length != 3)
+				{
+					Console.WriteLine("Pontosan 3 színt adj meg!");
+					continue;
+				}
+
+				int joHely = 0;
+				int joSzin = 0;
+
+				for(int i = 0; i < 3; i++)
+				{
+					if(szinTipp[i] == feladvany[i])
+					{
+						joHely++;
+					}
+					else if(szinTipp.Contains(feladvany[i]))
+					{
+						joSzin++;
+					}
+				}
+
+				Console.WriteLine($"Helyes szín, helyes helyen: {joHely}");
+				Console.WriteLine($"Helyes szín, rossz helyen: {joSzin}");
+
+				if(joHely == 3)
+				{
+					Console.WriteLine("Gratulálok, kitaláltad a színeket!");
+					talalt = true;
+				}
+			}
 		}
 	}
 }
