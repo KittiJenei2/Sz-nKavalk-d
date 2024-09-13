5b2e4263f9726cf4be3c5f28affc5a30914eecb1
Vivikee <102804665+svivien45@users.noreply.github.com>
2024-09-13 08:29:17 +0200

    tobb szin

--- Program.cs
+++ Program.cs
@@ -6,70 +6,82 @@ using System.Threading.Tasks;
 
 namespace szinkavalkad
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			List<string> alapSzinek = new List<string> { "piros", "kék", "sárga", "zöld" };
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> alapSzinek = new List<string> { "piros", "kék", "sárga", "zöld", "lila" };
 
-			Random rnd = new Random();
-			List<string> feladvany = new List<string>();
-			while(feladvany.Count < 3)
-			{
-				string color = alapSzinek[rnd.Next(alapSzinek.Count)];
-				if(!feladvany.Contains(color))
-				{
-					feladvany.Add(color);
-				}
-			}
+            Console.WriteLine("Add meg hány színt szeretnél kitalálni: ");
+            int darab = Convert.ToInt32(Console.ReadLine());
+            while (darab > 6)
+            {
+                Console.WriteLine("Maximum 6 színt választhatsz");
+                Console.WriteLine("Add meg hány színt szeretnél kitalálni: ");
+                darab = Convert.ToInt32(Console.ReadLine());
+            }
+            Random rnd = new Random();
+            List<string> feladvany = new List<string>();
+            while (feladvany.Count < darab)
+            {
+                string color = alapSzinek[rnd.Next(alapSzinek.Count)];
+                if (!feladvany.Contains(color))
+                {
+                    feladvany.Add(color);
+                }
+            }
 
-            Console.WriteLine("Találd ki a 3 színt a helyes sorrendben!");
-			Console.WriteLine("Választható színek: piros, kék, zöld, sárga");
-            Console.WriteLine("??   ??   ??");
+            Console.WriteLine($"Találd ki a {darab} színt a helyes sorrendben!");
+            Console.WriteLine("Választható színek: piros, kék, zöld, sárga");
+            for (int i = 0; i < darab; i++)
+            {
+                Console.Write("?? ");
+            }
             Console.WriteLine();
 
             bool talalt = false;
-			while(!talalt)
-			{
+            while (!talalt)
+            {
                 Console.WriteLine();
-                Console.WriteLine("Add meg a választott 3 színt (szóközzel elválasztva):");
-				string tipp = Console.ReadLine().ToLower();
-				string[] szinTipp = tipp.Split(' ');
+                Console.WriteLine($"Add meg a választott {darab} színt (szóközzel elválasztva):");
+                string tipp = Console.ReadLine().ToLower();
+                string[] szinTipp = tipp.Split(' ');
 
-				if(szinTipp.Length != 3)
-				{
-					Console.WriteLine("Pontosan 3 színt adj meg!");
-					continue;
-				}
+                if (szinTipp.Length != darab)
+                {
+                    Console.WriteLine($"Pontosan {darab} színt adj meg!");
+                    continue;
+                }
 
-				int joHely = 0;
-				int joSzin = 0;
+                int joHely = 0;
+                int joSzin = 0;
 
-				for(int i = 0; i < 3; i++)
-				{
-					if(szinTipp[i] == feladvany[i])
-					{
-						joHely++;
-					}
-					else if(szinTipp.Contains(feladvany[i]))
-					{
-						joSzin++;
-					}
-				}
+                for (int i = 0; i < darab; i++)
+                {
+                    if (szinTipp[i] == feladvany[i])
+                    {
+                        joHely++;
+                    }
+                    else if (szinTipp.Contains(feladvany[i]))
+                    {
+                        joSzin++;
+                    }
+                }
 
-				if(joHely == 3)
-				{
+                if (joHely == darab)
+                {
                     Console.WriteLine();
                     Console.WriteLine("-----------------------------");
                     Console.WriteLine("Gratulálok, kitaláltad a színeket!");
-					talalt = true;
-				}
-				else
-				{
-					Console.WriteLine($"Helyes szín, helyes helyen: {joHely}");
-					Console.WriteLine($"Helyes szín, rossz helyen: {joSzin}");
-				}
-			}
-		}
-	}
+                    Console.WriteLine("-----------------------------");
+                    talalt = true;
+                }
+                else
+                {
+                    Console.WriteLine($"Helyes szín, helyes helyen: {joHely}");
+                    Console.WriteLine($"Helyes szín, rossz helyen: {joSzin}");
+                }
+            }
+        }
+    }
 }
